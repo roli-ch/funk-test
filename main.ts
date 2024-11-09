84ecd9ecaf171963520b7c1c9d6c61c1e38b6424
Roli <129250229+roli-ch@users.noreply.github.com>
2024-11-09 08:59:33 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,30 +1,30 @@
-radio.onReceivedNumber(function (receivedNumber) {
+radio.onReceivedNumber(function on_received_number(receivedNumber: number) {
     basic.showNumber(receivedNumber)
     basic.showNumber(radio.receivedPacket(RadioPacketProperty.SignalStrength))
     serial.writeNumber(receivedNumber)
     serial.writeNumber(radio.receivedPacket(RadioPacketProperty.SignalStrength))
 })
-input.onButtonPressed(Button.A, function () {
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
     basic.showNumber(4)
     music.ringTone(262)
     pause(1000)
-music.stopAllSounds()
+    music.stopAllSounds()
     radio.sendString("test")
     radio.sendValue("name", 0)
 })
-radio.onReceivedString(function (receivedString) {
+radio.onReceivedString(function on_received_string(receivedString: string) {
     music.play(music.tonePlayable(349, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
     basic.showString(receivedString)
 })
-input.onButtonPressed(Button.B, function () {
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
     radio.sendNumber(2)
 })
-radio.onReceivedValue(function (name, value) {
+radio.onReceivedValue(function on_received_value(name: string, value: number) {
     serial.writeValue(name, value)
     basic.showString(name)
     basic.showNumber(value)
 })
 radio.setGroup(1)
-basic.forever(function () {
-	
+basic.forever(function on_forever() {
+    
 })
